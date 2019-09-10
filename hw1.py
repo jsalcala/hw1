8fb089c89491bbc4ebc11eb96c64c48a99004a74
jsalcala <jsalcala@uh.edu>
2019-09-10 08:41:52 -0500

    commit

---
+++ hw1.py
@@ -0,0 +1,13 @@
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+print("Hello")
+
+# Compute n factorial
+
+n = 6
+factn = 1
+for i in range(2, n + 1):
+    factn = factn * i
+
+print(factn)
